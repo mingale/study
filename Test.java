1301e40086378aa752a5699916ea5d650ca2aa4c
Mingael <promessa93@gmail.com>
2017-10-15 13:42:43 +0900

    '테스트입니다'

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.text.*;
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+Arrays.setAll(arr, (i) -> (int) (Math.random() * 5) + 1);
+
+    }
+}
