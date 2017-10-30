2781e201a4e9223fe9cefe3c1ab95f037a8f39b7
mingale <promessa93@gmail.com>
2017-10-30 20:10:46 +0900

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.text.*;
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = new int[5];
-Arrays.setAll(arr, (i) -> (int) (Math.random() * 5) + 1);
-//ㅁㄴㄹㄴㅇㅁㄻㄴㅇㄻㄻㄴㄹㄹㄻㄴㅇㄹㄴㅁㄹㅇㄹㅇsdafdfsdfsafsdf
-    }
-}
